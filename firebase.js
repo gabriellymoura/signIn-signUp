85da4c182c48c3a9fb37fef7b9120ce771c15799
Gabrielly Moura <gabriellymouta@gmail.com>
2020-12-06 19:04:37 -0300

    Add Autenticação

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCAhfS3U9ytA01njgIfApQ5zGBmJOkqWSI",
+    authDomain: "signin-singup-9a290.firebaseapp.com",
+    projectId: "signin-singup-9a290",
+    storageBucket: "signin-singup-9a290.appspot.com",
+    messagingSenderId: "992149310611",
+    appId: "1:992149310611:web:56cf2402eaded0f1439166",
+    measurementId: "G-ZC3ZK4BRKQ"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
---
+++ login.js
@@ -0,0 +1,40 @@
+ var btnSignIn = document.getElementById('btn-signin');
+ var btnSignUp = document.getElementById('btn-signup');
+ var email_in = document.getElementById('Username_in');
+ var senha_in = document.getElementById('Password_in');
+ var email_up = document.getElementById('Email_up');
+ var senha_up = document.getElementById('Password_up');
+
+ btnSignIn.addEventListener('click', function(){
+    firebase.auth().signInWithEmailAndPassword(email_in.value, senha_in.value)
+  .then((user) => {
+    // Signed in
+    // ...
+    alert("Usuario conectado!");
+    window.location.replace('main.html');
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+
+    alert(errorMessage);
+  });
+ });
+
+ btnSignUp.addEventListener('click', function(){
+    firebase.auth().createUserWithEmailAndPassword(email_up.value, senha_up.value)
+    .then((user) => {
+      // Signed in
+      // ...
+      alert("Usuario cadastrado!");
+      window.location.replace('main.html');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+      alert(errorMessage);
+
+    });
+ })
